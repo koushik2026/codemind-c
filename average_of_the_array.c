d46272fa5a6fc01fb7a9097101ba352678c815f0
koushik2026 <119729920+koushik2026@users.noreply.github.com>
2022-12-10 14:12:18 +0530

    #include<stdio.h> int main() {     long long int i,n;
        double avg,s=0;
        scanf("%lld",&n);
        int a[n];
        for(i=0;i<n;i++)
        {
            scanf("%lld",&a[i]);
        }
        for(i=0;i<n;i++)
        {
            s=s+a[i];
           avg=s/n;
        }
        //avg=s/n; 
        printf("%0.2f",avg);
    }

--- average_of_the_array.c
+++ average_of_the_array.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
 int main()
 {
-    int n,i;
-    float avg,s=0;
-    scanf("%d",&n);
+    long long int i,n;
+
+    double avg,s=0;
+    scanf("%lld",&n);
     int a[n];
     for(i=0;i<n;i++)
     {
-        scanf("%d",&a[i]);
+        scanf("%lld",&a[i]);
     }
     for(i=0;i<n;i++)
     {
         s=s+a[i];
-        avg=s/n;
+       avg=s/n;
     }
+    //avg=s/n; 
     printf("%0.2f",avg);
 }

26700b42c6af8e57bb15854cc1658f8d9e86927c
koushik2026 <119729920+koushik2026@users.noreply.github.com>
2022-12-21 20:31:54 +0530

    #include<stdio.h> int main() {     char V,I,B,G,Y,O,R,ch;     scanf("%c",&ch);     if(ch=='V')     {         printf("Violet");     }     else if(ch=='i')     {         printf("Indigo");     }     else if(ch=='B')     {         printf("Blue");     }     else if(ch=='G')     {         printf("Green");     }     else if(ch=='Y')     {         printf("Yellow");     }     else if(ch=='O')     {         printf("Orange");     }     else if(ch=='R')     {         printf("Red");     }     else     {         printf("-1");     }
    }

---
+++ Print_the_color_name.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    char V,I,B,G,Y,O,R,ch;
+    scanf("%c",&ch);
+    if(ch=='V')
+    {
+        printf("Violet");
+    }
+    else if(ch=='i')
+    {
+        printf("Indigo");
+    }
+    else if(ch=='B')
+    {
+        printf("Blue");
+    }
+    else if(ch=='G')
+    {
+        printf("Green");
+    }
+    else if(ch=='Y')
+    {
+        printf("Yellow");
+    }
+    else if(ch=='O')
+    {
+        printf("Orange");
+    }
+    else if(ch=='R')
+    {
+        printf("Red");
+    }
+    else 
+    {
+        printf("-1");
+    }
+    
+}
